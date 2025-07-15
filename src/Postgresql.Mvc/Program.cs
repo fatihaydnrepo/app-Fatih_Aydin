46a6e1ce16b2fc7b94f940a37d6aad6c8c0c2581
fatih <fatih-pc@fatih-pc-MacBook-Air.local>
2025-07-15 21:51:03 +0300

    123

--- src/Postgresql.Mvc/Program.cs
+++ src/Postgresql.Mvc/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
 
 namespace Postgresql.Mvc
 {
@@ -7,7 +10,32 @@ namespace Postgresql.Mvc
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Serilog konfigürasyonu
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.GrafanaLoki("http://loki.observability.svc.cluster.local:3100")
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
+            // OpenTelemetry Tracing
+            builder.Services.AddOpenTelemetry()
+                .WithTracing(tracerProviderBuilder =>
+                {
+                    tracerProviderBuilder
+                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Postgresql.Mvc"))
+                        .AddAspNetCoreInstrumentation()
+                        .AddHttpClientInstrumentation()
+                        .AddOtlpExporter(opt =>
+                        {
+                            opt.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317");
+                        });
+                });
+
+            var app = builder.Build();
+            app.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
