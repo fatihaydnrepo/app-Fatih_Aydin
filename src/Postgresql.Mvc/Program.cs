f1faaeec02c555b2869d4569ae4734c2e0656086
fatih <fatih-pc@fatih-pc-MacBook-Air.local>
2025-07-15 22:25:53 +0300

    2

--- src/Postgresql.Mvc/Program.cs
+++ src/Postgresql.Mvc/Program.cs
@@ -1,45 +1,50 @@
-﻿using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Serilog;
+﻿using Serilog;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Resources;
 
-namespace Postgresql.Mvc
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Serilog konfigürasyonu
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    // .WriteTo.GrafanaLoki("http://loki.observability.svc.cluster.local:3100") // Eğer build hatası verirse bu satırı yoruma al
+    .CreateLogger();
+
+builder.Host.UseSerilog();
+
+// OpenTelemetry Tracing
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracerProviderBuilder =>
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+        tracerProviderBuilder
+            .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Postgresql.Mvc"))
+            .AddAspNetCoreInstrumentation()
+            .AddHttpClientInstrumentation()
+            .AddOtlpExporter(opt =>
+            {
+                opt.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317");
+            });
+    });
 
-            // Serilog konfigürasyonu
-            Log.Logger = new LoggerConfiguration()
-                .WriteTo.Console()
-                .WriteTo.GrafanaLoki("http://loki.observability.svc.cluster.local:3100")
-                .CreateLogger();
+// MVC için gerekli servisler
+builder.Services.AddControllersWithViews();
 
-            builder.Host.UseSerilog();
+var app = builder.Build();
 
-            // OpenTelemetry Tracing
-            builder.Services.AddOpenTelemetry()
-                .WithTracing(tracerProviderBuilder =>
-                {
-                    tracerProviderBuilder
-                        .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("Postgresql.Mvc"))
-                        .AddAspNetCoreInstrumentation()
-                        .AddHttpClientInstrumentation()
-                        .AddOtlpExporter(opt =>
-                        {
-                            opt.Endpoint = new Uri("http://otel-collector.observability.svc.cluster.local:4317");
-                        });
-                });
-
-            var app = builder.Build();
-            app.Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
